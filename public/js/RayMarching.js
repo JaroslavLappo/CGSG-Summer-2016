df92433ab5fc60c95600281ca08b5ae2a4e3006c
JaroslavLappo <jar102938@gmail.com>
2016-07-04 14:05:36 +0400

    Resolved problemes

--- public/js/RayMarching.js
+++ public/js/RayMarching.js
@@ -20,6 +20,8 @@ var lplayer = {
 };
 var lplayer_num;
 var socket;
+var Acceleration = vec3.create();
+var OldAcceleration = vec3.create();
 
 function NetworkInit(name) {
     socket = io();
@@ -74,16 +76,16 @@ function NetworkInit(name) {
     });
 
 
-    function PosSend(timeout) {
-        lplayer.pos = Camera.Pos;
-        if (!vec3.exactEquals(oldpos, lplayer.pos)) {
-            socket.emit('send_pos', lplayer.pos);
-            vec3.copy(oldpos, lplayer.pos);
+    function AccelerationSend(timeout) {
+        if (!vec3.exactEquals(OldAcceleration, Acceleration)) {
+            console.log("send acceleration" + Acceleration);
+            socket.emit('send_acceleration', Acceleration);
+            vec3.copy(OldAcceleration, Acceleration);
         }
-        setTimeout(PosSend, timeout, timeout);
+        setTimeout(AccelerationSend, timeout, timeout);
     }
 
-    PosSend(100);
+    AccelerationSend(100);
 }
 
 var gl;
@@ -204,8 +206,6 @@ function GetShader(gl, FileName) {
 }
 
 var shaderProgram;
-var CamPos = vec3.create();
-var CamDir = vec3.create();
 
 function InitShaders() {
     var fragmentShader = GetShader(gl, "shaders/shader.frag");
@@ -256,24 +256,23 @@ function SetMatrixUniforms() {
     if (CameraMode == "Circle")
         vec3.set(TimeVec, Math.cos(time), Math.sin(time), (Math.sin(time / 2) + 1) / 2);
 
+    if (CameraMode != "Game") {
+        vec3.subtract(Camera.Dir, LookAt, Pos);
+        vec3.copy(Camera.Pos, Pos);
+    }
+
     Pos[0] = TimeVec[0] * 0.4;
     Pos[1] = TimeVec[2] * 0.6;
     Pos[2] = TimeVec[1] * 0.4;
 
-    vec3.subtract(CamDir, LookAt, Pos);
-
-    vec3.copy(CamPos, Pos);
-
     if (CameraMode == "Game") {
-        vec3.copy(CamDir, Camera.Dir);
-        vec3.copy(CamPos, Camera.Pos);
         vec3.set(TimeVec, Math.cos(time * 4 / 3), Math.sin(time * 4 / 5), (Math.sin(time * 4 / 2) + 1) / 2);
     }
 
-    vec3.normalize(CamDir, CamDir);
+    vec3.normalize(Camera.Dir, Camera.Dir);
 
-    gl.uniform3f(shaderProgram.CamPosUnifrom, CamPos[0], CamPos[1], CamPos[2]);
-    gl.uniform3f(shaderProgram.CamViewUnifrom, CamDir[0], CamDir[1], CamDir[2]);
+    gl.uniform3f(shaderProgram.CamPosUnifrom, Camera.Pos[0], Camera.Pos[1], Camera.Pos[2]);
+    gl.uniform3f(shaderProgram.CamViewUnifrom, Camera.Dir[0], Camera.Dir[1], Camera.Dir[2]);
     gl.uniform1f(shaderProgram.ProjDistUnifrom, 1);
     gl.uniform1f(shaderProgram.TimeUnifrom, time);
     gl.uniform3f(shaderProgram.LightPointUnifrom[0], 0.5 * TimeVec[1], 1 - TimeVec[2], 0.5 * TimeVec[0]);
@@ -415,13 +414,17 @@ function controlsKeyboard(event) {
 
     if (key == 87)
 <<<<<<< HEAD
+<<<<<<< HEAD
         CameraTranslate(0, 0, -speed);
+=======
+        vec3.set(Acceleration, 0, 0, -speed);
+>>>>>>> parent of ab1476b... Revert "Modifed physics of moving."
     if (key == 65)
-        CameraTranslate(-speed, 0, 0);
+        vec3.set(Acceleration, -speed, 0, 0);
     if (key == 83)
-        CameraTranslate(0, 0, speed);
+        vec3.set(Acceleration, 0, 0, speed);
     if (key == 68)
-        CameraTranslate(speed, 0, 0);
+        vec3.set(Acceleration, speed, 0, 0);
     //console.log("Pressed: " + key);
 =======
         vec3.multiply(Acceleration, Camera.Dir, vec3.fromValues(1, 1, 1));
